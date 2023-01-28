e8b0ed98facef0a780cc36cd5b38e861de608682
minexclamationpoint <123146227+minexclamationpoint@users.noreply.github.com>
2023-01-28 14:07:58 -0500

    Update order.py
    added coffee through steel functions
    unsure about cost atm

--- order.py
+++ order.py
@@ -12,3 +12,23 @@ class Order:
         self.total_cost += 7.5
         self.items += ('lemonade')
         print("Added lemonade!")
+    def add_coffee(self):
+        self.total_cost += 7.5
+        self.items += ('coffee')
+        print("Added coffee!")
+    def add_fries(self):
+        self.total_cost += 7.5
+        self.items += ('fries')
+        print("Added fries!")
+    def add_salad(self):
+        self.total_cost += 7.5
+        self.items += ('salad')
+        print("Added salad!")
+   def add_wrap(self):
+        self.total_cost += 7.5
+        self.items += ('wrap')
+        print("Added wrap!")
+   def add_steel(self):
+        self.total_cost += 7.5
+        self.items += ('10 lbs of steel')
+        print("Added 10 lbs of steel!")
